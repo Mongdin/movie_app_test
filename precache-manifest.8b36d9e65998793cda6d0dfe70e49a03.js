8463722d508857845851870dca0bc6fafb715c69
Mongdin <ajfqhkh@gmail.com>
2020-02-02 10:35:35 +0900

    Updates

---
+++ precache-manifest.8b36d9e65998793cda6d0dfe70e49a03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3de5e21c1031b718ae1a63a68cd0b288",
+    "url": "/movie_app_test/index.html"
+  },
+  {
+    "revision": "d480b12ac021138e8267",
+    "url": "/movie_app_test/static/css/main.86a7abee.chunk.css"
+  },
+  {
+    "revision": "a0f2b829ca7a64e5fff0",
+    "url": "/movie_app_test/static/js/2.9d455fbe.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app_test/static/js/2.9d455fbe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d480b12ac021138e8267",
+    "url": "/movie_app_test/static/js/main.85e299f1.chunk.js"
+  },
+  {
+    "revision": "d3c1557c4aeb6a14717d",
+    "url": "/movie_app_test/static/js/runtime-main.4ba950a6.js"
+  }
+]);
